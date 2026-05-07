9ca16466e717793cfac07972f717184de0b645ad
shafayatGit <shafayathossain.ielts@gmail.com>
2026-05-07 22:36:47 +0600

    feat: doctor created with specialities

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,6 @@ const router = Router();
 
 router.use("/specialities", SpecialityRoutes);
 router.use("/auth", AuthRoutes);
-router.use("/doctors", userRoutes);
+router.use("/users", userRoutes);
 
 export const IndexRoutes = router;
