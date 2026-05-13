89e9e529f83d86739f6f6ac3ebaea7cd9ecbd3f4
shafayatGit <shafayathossain.ielts@gmail.com>
2026-05-13 20:10:33 +0600

    feat: admin service created with schema

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -132,7 +132,7 @@ const createDoctor = async (payload: ICreaeteDoctorPayload) => {
 };
 
 const createAdmin = async (payload: ICreateAdminPayload) => {
-  //TODO: Validate who is creating the admin user. Only super admin can create admin user and only super admin can create super admin user but admin user cannot create super admin user
+  //TODO: Validate who is creating the admin user. Only super admin can create admin user and only super admin can create super admin user but admin user cannot create super admin user.
 
   const userExists = await prisma.user.findUnique({
     where: {
